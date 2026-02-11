443da6ab9ce8a8016d8ab6700cc78e48b5938580
Akela <muradsolomanov8@gmail.com>
2026-02-11 07:37:27 +0300

    Урок по псевдослучайным числам

---
+++ Leassons/LL18_Generate_random_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int r_1 = rand();
+    int r_2 = rand();
+
+    printf("%d, %d, %d, %d, %d\n", r_1, r_2, rand(), rand(), rand());
+
+
+
+    return 0;
+}
